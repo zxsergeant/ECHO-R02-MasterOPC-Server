51fa9da88c576e3f66d9b21b925f52d1328cd96a
zxsergeant <zxsergeant@yandex.ru>
2019-03-27 16:22:42 +0400

    Update Archive_Q_day.lua

--- RS485_UDFC/Archive_Q_day.lua
+++ RS485_UDFC/Archive_Q_day.lua
@@ -2,7 +2,6 @@
 -- Функция (69) приёма n строк посуточного архива, начиная с i-й
 ---------------------------------------------------------------------------
 function ReadECHO69(nlin)
-  --server.Message( "Start");
   local send={}; --массив отправляемых чисел
   table.insert(send,Address); --адрес
   table.insert(send,0x69); --функция
@@ -17,41 +16,29 @@ function ReadECHO69(nlin)
     --увеличиваем маску принимаемого запросана на величину строк запроса
     table.insert(destmask,"int32:1:0123");
     table.insert(destmask,"byte:3");
-    --server.Message( " цикл=",nlin1);
     nlin1=nlin1-1;
   until nlin1<=0
   --определяем размер получаемых данных в байтах
   local err,len;
   local n=0;
   local nbyte=5+nlin*7;
-  --
-  repeat
+    repeat
     --посылка и получение запроса в устройство
     err,dest,len=server.SendAndReceiveDataByMask(2,5,sendmask,send,destmask,nbyte);
     n=n+1;
     --условие выхода - корректный ответ или превышение запросов
   until err>=0 or n>=server.GetCurrentDeviceRetry()
-  --
-  --server.Message( " err=",err," len=",len);
-  --
-  --server.Message( "func err=",err);
   if err>=0 then
     --local multipler=server.ReadTagByRelativeName("multipler");
     local err1,multipler=GetMultipler();
     nlin1=nlin;
-    --
     local a=4;
     repeat
       dest[a]=dest[a]*multipler;
-      --    server.Message( " значение=",dest[a]," a=",a);
-
-      --    server.Message( " data=",2000+FromBCD(dest[a+3]),".",FromBCD(dest[a+2]),".",FromBCD(dest[a+1]),".",0);
       a=a+4;
       nlin1=nlin1-1;
     until nlin1<=0
-    --
     return err,dest;
-    --
   else
     read=false;
     return -1,0; --запрос некорректен, возвращаем соответствующий флаг
@@ -94,8 +81,6 @@ function GetMultipler()
   --обработка результатов
   if err>=0 then
     dest[8] = (10^ (dest[8] - 3 ));
-    --dest[6] = dest[6] * dest[8];
-    --server.Message("MultiFunc=",dest[8]);
     return true,dest[8];
   else
     return false,0; --запрос некорректен, возвращаем соответствующий флаг
@@ -105,71 +90,48 @@ function GetMultipler()
 -- Initialization
 ---------------------------------------------------------------------------
 function OnInit()
-
     Address =  server.ReadSubDeviceExtProperty("Адрес");
     read=false;
-
 end
 ---------------------------------------------------------------------------
 -- Uninitialization
 ---------------------------------------------------------------------------
 function OnClose()
 
-
 end
---
-
-
 ---------------------------------------------------------------------------
 -- Processing
 ---------------------------------------------------------------------------
 function OnRead()
   local errok;--={}; --массив полученных чисел
-  --server.Message( "...reading");
-
   if read==false
   then do
     local dest={}; --массив полученных чисел
     local deep=server.ReadSubDeviceExtProperty("Суток посуточного архива");
-    --
-    --deep=2;
     errok,dest=ReadECHO69(deep); --Запрос данных с прибора
-
-    if errok<=0 then return;
-    end;
-
+      if errok<=0 then return;
+      end;
     local a=4;
     repeat
-      --
       Err,timesec = time.PackTime(2000+FromBCD(dest[a+3]),FromBCD(dest[a+2]),FromBCD(dest[a+1]),00,00,00);
       ts = time.TimeToTimeStamp(timesec,0);
       server.WriteCurrentTagToHda(dest[a],OPC_QUALITY_GOOD,ts)
       a=a+4;
       deep=deep-1;
     until deep<=0
-    --
-    --server.Message( "then");
     read=true;
     end;
-    ------------------------
   else do
     local dest={}; --массив полученных чисел
     local deep=1;
     errok,dest=ReadECHO69(deep); --Запрос данных с прибора
-
     if errok<=0 then return;
     end;
-
     local a=4;
     Err,timesec = time.PackTime(2000+FromBCD(dest[a+3]),FromBCD(dest[a+2]),FromBCD(dest[a+1]),00,00,00);
     ts = time.TimeToTimeStamp(timesec,0);
-
-    --server.Message( "else");
     server.WriteCurrentTag(dest[4],OPC_QUALITY_GOOD,ts);
     server.WriteCurrentTagToHda(dest[a],OPC_QUALITY_GOOD,ts)
-
     end;
-
     end;
-    --server.Message( "end err=",errok);
   end
