26c4fe0a8a983c05e77f1f8a89987c047d4b0369
zxsergeant <zxsergeant@yandex.ru>
2019-03-27 16:13:36 +0400

    Update Archive_Q_day.lua

--- RS232/Archive_Q_day.lua
+++ RS232/Archive_Q_day.lua
@@ -2,7 +2,6 @@
 -- Функция приёма n строк посуточного архива, начиная с i-й
 ---------------------------------------------------------------------------
 function ReadECHOi5(nlin)
-  --server.Message( "Start");
   local send={}; --массив отправляемых чисел
   table.insert(send,0xAA); --адрес
   table.insert(send,0x05); --функция
@@ -17,7 +16,6 @@ function ReadECHOi5(nlin)
     --увеличиваем маску принимаемого запросана на величину строк запроса
     table.insert(destmask,"int32:1:0123");
     table.insert(destmask,"byte:3");
-    --server.Message( " цикл=",nlin1);
     nlin1=nlin1-1;
   until nlin1<=0
   --определяем размер получаемых данных в байтах
@@ -31,10 +29,6 @@ function ReadECHOi5(nlin)
     n=n+1;
     --условие выхода - корректный ответ или превышение запросов
   until err>=0 or n>=server.GetCurrentDeviceRetry()
-  --
-  --server.Message( " err=",err," len=",len);
-  --
-  --server.Message( "func err=",err);
   if err>=0 then
     --local multipler=server.ReadTagByRelativeName("multipler");
     local err1,multipler=GetMultipler();
@@ -43,9 +37,6 @@ function ReadECHOi5(nlin)
     local a=1;
     repeat
       dest[a]=dest[a]*multipler;
-      --    server.Message( " значение=",dest[a]," a=",a);
-
-      --    server.Message( " data=",2000+FromBCD(dest[a+3]),".",FromBCD(dest[a+2]),".",FromBCD(dest[a+1]),".",0);
       a=a+4;
       nlin1=nlin1-1;
     until nlin1<=0
@@ -93,9 +84,7 @@ local send={}; --массив отправляемых чисел
   until err>=0 or n>=server.GetCurrentDeviceRetry()
   --обработка результатов
   if err>=0 then
-    --if dest[5]>=0 and dest[5]<=5 then
     dest[5] = (10^ (dest[5] - 3 ));
-    --server.Message( "funcmult=",dest[5]);
     return true,dest[5];
   else
     return false,0; --запрос некорректен, возвращаем соответствующий флаг
@@ -125,14 +114,10 @@ end
 ---------------------------------------------------------------------------
 function OnRead()
   local errok;--={}; --массив полученных чисел
-  --server.Message( "...reading");
-
   if read==false
   then do
     local dest={}; --массив полученных чисел
     local deep=server.ReadSubDeviceExtProperty("Суток посуточного архива");
-    --
-    --deep=2;
     errok,dest=ReadECHOi5(deep); --Запрос данных с прибора
 
     if errok<=0 then return;
@@ -147,8 +132,6 @@ function OnRead()
       a=a+4;
       deep=deep-1;
     until deep<=0
-    --
-    --server.Message( "then");
     read=true;
     end;
     ------------------------
@@ -164,12 +147,9 @@ function OnRead()
     Err,timesec = time.PackTime(2000+FromBCD(dest[a+3]),FromBCD(dest[a+2]),FromBCD(dest[a+1]),00,00,00);
     ts = time.TimeToTimeStamp(timesec,0);
 
-    --server.Message( "else");
     server.WriteCurrentTag(dest[4],OPC_QUALITY_GOOD,ts);
     server.WriteCurrentTagToHda(dest[a],OPC_QUALITY_GOOD,ts)
 
     end;
-
     end;
-    --server.Message( "end err=",errok);
   end
